c2b492c811cd9ce163ccd004aa876f1bd34b1cbb
Damian Escobar <damianem611@gmail.com>
2024-11-21 14:52:07 +0100

    clase no terminada

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./Home.jsx"
+
 const App = () => {
+  const [data, setData] = useState(null)
+  const urlApi = "http://localhost:3000/"
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(urlApi)
+      const resData = await response.json()
+      setData(resData)
+
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
+   useEffect(() => {
+    fetchData()
+   }, [])
 
   return (
-  <></>      
+    <Router>
+      <div>
+        <nav>
+          <Link to="/">Inicio</Link>
+        </nav>
+        {data === null ? (<div>Cargando...</div>)
+        :      
+          <Routes>
+            <Route path="/" element={<Home data={data} />} />
+          </Routes>
+        }
+      </div>
+    </Router>
   )
 };
 
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,17 @@
-const Home = () => {
+import { Link } from 'react-router-dom'
+
+const Home = (data) => {
+
   return (
     <>
-    
+    <h2>Lista de datos</h2>
+    <ul>
+    {data.map(item => (
+      <li key={item._id}>
+        <Link to={`/${item._id}`}>{item.title}</Link>
+      </li>
+    ))}
+    </ul>
     </>
   )
 };
